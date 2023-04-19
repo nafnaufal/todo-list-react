aa96c9208b44391a6f0232d58b84a62ffb0f42a3
nafnaufal <naufalanbial@gmail.com>
2023-04-20 03:30:15 +0700

    complete todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todoItem, setTodoItem] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setTodoItem(currentTodoItem => {
+      return [
+        ...currentTodoItem, {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false
+        }
+      ]
+    })
+    setNewItem("")
+  }
+  const completedItem = (id, checked) => {
+    setTodoItem(currentTodoItem => {
+      return currentTodoItem.map(item => {
+        if (item.id === id) {
+          return { ...item, completed:checked }
+        }
+        return item
+      })
+    })
+
+  }
+  
+  const deletTodo = (id) => {
+    setTodoItem(currentTodoItem => {
+      return currentTodoItem.filter(item => item.id !== id);
+    })
+
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <form onSubmit={handleSubmit} action="">
+        <label htmlFor="addField">Add new</label>
+        <input
+          type="text"
+          id="addField"
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+        />
+        <input type="submit" value="Add" />
+      </form>
+      <h1>To-Do List</h1>
+      <ul>
+        {todoItem.length == 0 && "List kosong"}
+        {todoItem.map(item => {
+          return <li key={item.id}>
+            <input type="checkbox"
+              id="i1"
+              checked={item.completed}
+              onChange={(e) => completedItem(item.id, e.target.checked)}
+            />
+            {item.title}
+            <button onClick={() => deletTodo(item.id)}>Delete</button>
+          </li>
+        })}
+      </ul>
+    </>
   )
 }
-
-export default App
